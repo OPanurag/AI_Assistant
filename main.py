c09df4968c84d351ad26f24d063682870ac68d21
Anurag Mishra <101906072+OPanurag@users.noreply.github.com>
2022-07-20 19:20:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,110 @@
-import datetime
-import speech_recognition as sr
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtGui import QMovie
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PySide2.QtUiTools import loadUiType
+from PyQt5.uic import loadUiType
 import pyttsx3
-import wolframalpha
+import speech_recognition as sr
+import os
+import time
 import webbrowser
-import wikipedia
+import datetime
+import PyQt5
+
+flags = PyQt5.QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 180)
 
 
-# Speech engine initialisation
-engine = pyttsx3.init()
-voice = engine.getProperty('voice')
-engine.setProperty('voice', voice[0])    # 0 = male, 1 = female
-activation_word = "iva"     # 'iva' stands for "Intelligent Virtual assistant"
-
-
-def parse_command():
-    listener = sr.Recognizer()
-    print("Listening...")
-
-    with sr.Microphone as source:
-        listener.pause_threshold = 2
-        input_speech = listener.listen(source)
-
-    try:
-        print("Recognizing command...")
-        query_1 = listener.recognize_google(input_speech, language='en_gb')
-        print("The command was {0}".format(query_1))
-
-    except Exception as exception:
-        print("Could not hear you")
-        speak("Could not hear you")
-        print(exception)
-        return 'None'
-
-    return query
-
-
-def speak(text, rate=150):
-    engine.setProperty('rate', rate)
-    engine.say(text)
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
 
 
-# Main Loop
-if __name__ == '__main__':
-    speak("Hey there. What can I do for you?", 190)
+def wish():
+    hour = int(datetime.datetime.now().hour)
+    if 0 <= hour < 12:
+        speak("Good morning")
+    elif 12 <= hour < 18:
+        speak("Good Afternoon")
+    else:
+        speak("Good night")
 
-    while True:     # Parse as list
-        query = parse_command().lower().split()
 
-        if query[0] == activation_word:
-            query.pop(0)
+class mainT(QThread):
+    def __init__(self):
+        super(mainT, self).__init__()
 
-            if query[0] == 'say':
-                if 'hello'.lower() in query:
-                    speak('Greetings')
-                else:
-                    query.pop(0)    # Remove 'Say' from the list
-                    speech = '  '.join(query)
-                    speak(speech)
+    def run(self):
+        self.JARVIS()
+
+    def STT(self):
+        R = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listning...........")
+            audio = R.listen(source)
+        try:
+            print("Recog......")
+            text = R.recognize_google(audio, language='en-in')
+            print(">> ", text)
+        except Exception:
+            speak("Sorry Speak Again")
+            return "None"
+        text = text.lower()
+        return text
+
+    def JARVIS(self):
+        wish()
+        while True:
+            self.query = self.STT()
+            if 'good bye' in self.query:
+                sys.exit()
+            elif 'open google' in self.query:
+                webbrowser.open('www.google.co.in')
+                speak("opening google")
+            elif 'open youtube' in self.query:
+                webbrowser.open("www.youtube.com")
+            elif 'play music' in self.query:
+                speak("playing music from pc")
+                self.music_dir = "./music"
+                self.musics = os.listdir(self.music_dir)
+                os.startfile(os.path.join(self.music_dir, self.musics[0]))
+
+
+FROM_MAIN, _ = PyQt5.loadUiType(os.path.join(os.path.dirname(__file__), "./scifi.ui"))
+
+
+class Main(QMainWindow, FROM_MAIN):
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        self.setupUi(self)
+        self.setFixedSize(1920, 1080)
+        self.label_7 = QLabel
+        self.exitB.setStyleSheet("background-image:url(./lib/exit.png);\n"
+                                 "border:none;")
+        self.exitB.clicked.connect(self.close)
+        self.setWindowFlags(flags)
+        Dspeak = mainT()
+        self.label_7 = QMovie("./lib/gifloader.gif", QByteArray(), self)
+        self.label_7.setCacheMode(QMovie.CacheAll)
+        self.label_4.setMovie(self.label_7)
+        self.label_7.start()
+
+        self.ts = time.strftime("%A, %d %B")
+
+        Dspeak.start()
+        self.label.setPixmap(QPixmap("./lib/tuse.png"))
+        self.label_5.setText("<font size=8 color='white'>" + self.ts + "</font>")
+        self.label_5.setFont(QFont(QFont('Acens', 8)))
+
+
+app = QtWidgets.QApplication(sys.argv)
+main = Main()
+main.show()
+exit(app.exec_())
