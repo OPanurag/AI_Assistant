374bb609e8b520a997da6592613b6f4ebb834d1d
Anurag Mishra <101906072+OPanurag@users.noreply.github.com>
2022-07-20 21:05:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,166 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtGui import QMovie
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PySide2.QtUiTools import loadUiType
-from PyQt5.uic import loadUiType
 import pyttsx3
 import speech_recognition as sr
-import os
-import time
 import webbrowser
 import datetime
-import PyQt5
+import wikipedia
 
-flags = PyQt5.QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
-engine.setProperty('rate', 180)
+# this method is for taking the commands
+# and recognizing the command from the
+# speech_Recognition module we will use
+# the recognizer method for recognizing
+
+def takeCommand():
+    r = sr.Recognizer()
+
+    # from the speech_Recognition module
+    # we will use the Microphone module
+    # for listening the command
+    with sr.Microphone() as source:
+        print('Listening')
+
+        # seconds of non-speaking audio before
+        # a phrase is considered complete
+        r.pause_threshold = 0.7
+        audio = r.listen(source)
+
+        # Now we will be using the try and catch
+        # method so that if sound is recognized
+        # it is good else we will have exception
+        # handling
+        try:
+            print("Recognizing")
+
+            # for Listening the command in indian
+            # english we can also use 'hi-In'
+            # for hindi recognizing
+            Query = r.recognize_google(audio, language='en-in')
+            print("the command is printed=", Query)
+
+        except Exception as e:
+            print(e)
+            print("Say that again sir")
+            return "None"
+
+        return Query
 
 
 def speak(audio):
+    engine = pyttsx3.init()
+    # getter method(gets the current value
+    # of engine property)
+    voices = engine.getProperty('voices')
+
+    # setter method .[0]=male voice and
+    # [1]=female voice in set Property.
+    engine.setProperty('voice', voices[0].id)
+
+    # Method for the speaking of the assistant
     engine.say(audio)
+
+    # Blocks while processing all the currently
+    # queued commands
     engine.runAndWait()
 
 
-def wish():
-    hour = int(datetime.datetime.now().hour)
-    if 0 <= hour < 12:
-        speak("Good morning")
-    elif 12 <= hour < 18:
-        speak("Good Afternoon")
-    else:
-        speak("Good night")
+def tellDay():
+    # This function is for telling the
+    # day of the week
+    day = datetime.datetime.today().weekday() + 1
+
+    # this line tells us about the number that will help us in telling the day
+
+    Day_dict = {1: 'Monday', 2: 'Tuesday',
+                3: 'Wednesday', 4: 'Thursday',
+                5: 'Friday', 6: 'Saturday',
+                7: 'Sunday'}
+
+    if day in Day_dict.keys():
+        day_of_the_week = Day_dict[day]
+        print(day_of_the_week)
+        speak("The day is " + day_of_the_week)
 
 
-class mainT(QThread):
-    def __init__(self):
-        super(mainT, self).__init__()
+def tellTime():
+    # This method will give the time
+    time = str(datetime.datetime.now())
 
-    def run(self):
-        self.JARVIS()
-
-    def STT(self):
-        R = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listning...........")
-            audio = R.listen(source)
-        try:
-            print("Recog......")
-            text = R.recognize_google(audio, language='en-in')
-            print(">> ", text)
-        except Exception:
-            speak("Sorry Speak Again")
-            return "None"
-        text = text.lower()
-        return text
-
-    def JARVIS(self):
-        wish()
-        while True:
-            self.query = self.STT()
-            if 'good bye' in self.query:
-                sys.exit()
-            elif 'open google' in self.query:
-                webbrowser.open('www.google.co.in')
-                speak("opening google")
-            elif 'open youtube' in self.query:
-                webbrowser.open("www.youtube.com")
-            elif 'play music' in self.query:
-                speak("playing music from pc")
-                self.music_dir = "./music"
-                self.musics = os.listdir(self.music_dir)
-                os.startfile(os.path.join(self.music_dir, self.musics[0]))
+    # the time will be displayed like
+    # this "2020-06-05 17:50:14.582630"
+    # and then after slicing we can get time
+    print(time)
+    hour = time[11:13]
+    minutes = time[14:16]
+    speak("The time is sir" + hour + "Hours and" + minutes + "Minutes")
 
 
-FROM_MAIN, _ = PyQt5.loadUiType(os.path.join(os.path.dirname(__file__), "./scifi.ui"))
+def hello():
+    # This function is for when the assistant
+    # is called it will say hello and then
+    # take query
+    speak("Hello sir, What can I do for you")
 
 
-class Main(QMainWindow, FROM_MAIN):
-    def __init__(self, parent=None):
-        super(Main, self).__init__(parent)
-        self.setupUi(self)
-        self.setFixedSize(1920, 1080)
-        self.label_7 = QLabel
-        self.exitB.setStyleSheet("background-image:url(./lib/exit.png);\n"
-                                 "border:none;")
-        self.exitB.clicked.connect(self.close)
-        self.setWindowFlags(flags)
-        Dspeak = mainT()
-        self.label_7 = QMovie("./lib/gifloader.gif", QByteArray(), self)
-        self.label_7.setCacheMode(QMovie.CacheAll)
-        self.label_4.setMovie(self.label_7)
-        self.label_7.start()
+def Take_query():
+    # calling the Hello function for
+    # making it more interactive
+    hello()
 
-        self.ts = time.strftime("%A, %d %B")
+    # This loop is infinite as it will take
+    # our queries continuously until and unless
+    # we do not say bye to exit or terminate
+    # the program
+    while True:
 
-        Dspeak.start()
-        self.label.setPixmap(QPixmap("./lib/tuse.png"))
-        self.label_5.setText("<font size=8 color='white'>" + self.ts + "</font>")
-        self.label_5.setFont(QFont(QFont('Acens', 8)))
+        # taking the query and making it into
+        # lower case so that most of the times query matches, and we get the perfect output
+
+        query = takeCommand().lower()
+        if "open youtube" in query:
+            speak("Opening youtube ")
+
+            # in the open method we just to give the link of the website, and it automatically opens
+            # it in your default browser
+            webbrowser.open("www.youtube.com")
+            continue
+
+        elif "open google" in query:
+            speak("Opening Google ")
+            webbrowser.open("www.google.com")
+            continue
+
+        elif "which day it is" in query:
+            tellDay()
+            continue
+
+        elif "tell me the time" in query:
+            tellTime()
+            continue
+
+        # this will exit and terminate the program
+
+        elif "bye" in query:
+            speak("Thank You and Good Bye")
+            exit()
+
+        elif "from wikipedia" in query:
+
+            # if anyone wants to have information from wikipedia
+            speak("Checking the wikipedia ")
+            query = query.replace("wikipedia", "")
+
+            # it will give the summary of 4 lines from
+            # wikipedia we can increase and decrease
+            # it also.
+            result = wikipedia.summary(query, sentences=4)
+            speak("According to wikipedia")
+            speak(result)
+
+        elif "tell me your name" in query:
+            speak("I am IVA, Your desktop Assistant")
 
 
-app = QtWidgets.QApplication(sys.argv)
-main = Main()
-main.show()
-exit(app.exec_())
+if __name__ == '__main__':
+    # main method for executing
+    # the functions
+    Take_query()
