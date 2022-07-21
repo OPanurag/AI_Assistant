2149bd8e9e31e663d0215f7ecb35ff024b32f930
Anurag Mishra <101906072+OPanurag@users.noreply.github.com>
2022-07-21 15:30:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,23 @@ import speech_recognition as sr
 import webbrowser
 import datetime
 import wikipedia
+from tkinter import *
 
-
-# this method is for taking the commands
-# and recognizing the command from the
-# speech_Recognition module we will use
-# the recognizer method for recognizing
 
 def takeCommand():
     r = sr.Recognizer()
 
-    # from the speech_Recognition module
-    # we will use the Microphone module
-    # for listening the command
     with sr.Microphone() as source:
         print('Listening')
-
-        # seconds of non-speaking audio before
-        # a phrase is considered complete
         r.pause_threshold = 0.7
         audio = r.listen(source)
 
-        # Now we will be using the try and catch
-        # method so that if sound is recognized
-        # it is good else we will have exception
-        # handling
         try:
             print("Recognizing")
 
-            # for Listening the command in indian
-            # english we can also use 'hi-In'
-            # for hindi recognizing
+            # for command in indian english use 'hi-In'
             Query = r.recognize_google(audio, language='en-in')
-            print("the command is printed=", Query)
+            print("the command is '{}'".format(Query))
 
         except Exception as e:
             print(e)
@@ -47,28 +31,19 @@ def takeCommand():
 
 def speak(audio):
     engine = pyttsx3.init()
-    # getter method(gets the current value
-    # of engine property)
     voices = engine.getProperty('voices')
 
-    # setter method .[0]=male voice and
-    # [1]=female voice in set Property.
-    engine.setProperty('voice', voices[0].id)
+    # [0]=male & [1]=female voice
 
-    # Method for the speaking of the assistant
+    engine.setProperty('voice', voices[1].id)
+
     engine.say(audio)
 
-    # Blocks while processing all the currently
-    # queued commands
     engine.runAndWait()
 
 
 def tellDay():
-    # This function is for telling the
-    # day of the week
     day = datetime.datetime.today().weekday() + 1
-
-    # this line tells us about the number that will help us in telling the day
 
     Day_dict = {1: 'Monday', 2: 'Tuesday',
                 3: 'Wednesday', 4: 'Thursday',
@@ -82,45 +57,26 @@ def tellDay():
 
 
 def tellTime():
-    # This method will give the time
     time = str(datetime.datetime.now())
-
-    # the time will be displayed like
-    # this "2020-06-05 17:50:14.582630"
-    # and then after slicing we can get time
     print(time)
     hour = time[11:13]
     minutes = time[14:16]
-    speak("The time is sir" + hour + "Hours and" + minutes + "Minutes")
+    speak("Sir, the time is" + hour + "Hours and" + minutes + "Minutes")
 
 
 def hello():
-    # This function is for when the assistant
-    # is called it will say hello and then
-    # take query
     speak("Hello sir, What can I do for you")
 
 
 def Take_query():
-    # calling the Hello function for
-    # making it more interactive
     hello()
 
-    # This loop is infinite as it will take
-    # our queries continuously until and unless
-    # we do not say bye to exit or terminate
-    # the program
     while True:
-
-        # taking the query and making it into
-        # lower case so that most of the times query matches, and we get the perfect output
 
         query = takeCommand().lower()
         if "open youtube" in query:
             speak("Opening youtube ")
 
-            # in the open method we just to give the link of the website, and it automatically opens
-            # it in your default browser
             webbrowser.open("www.youtube.com")
             continue
 
@@ -133,11 +89,9 @@ def Take_query():
             tellDay()
             continue
 
-        elif "tell me the time" in query:
+        elif "what time is it now" in query:
             tellTime()
             continue
-
-        # this will exit and terminate the program
 
         elif "bye" in query:
             speak("Thank You and Good Bye")
@@ -145,22 +99,32 @@ def Take_query():
 
         elif "from wikipedia" in query:
 
-            # if anyone wants to have information from wikipedia
             speak("Checking the wikipedia ")
             query = query.replace("wikipedia", "")
 
-            # it will give the summary of 4 lines from
-            # wikipedia we can increase and decrease
-            # it also.
             result = wikipedia.summary(query, sentences=4)
             speak("According to wikipedia")
             speak(result)
 
-        elif "tell me your name" in query:
+        elif "Who are you" in query:
             speak("I am IVA, Your desktop Assistant")
 
 
+def main_screen():
+
+    screen = Tk()
+    screen.title('Intelligent Virtual Assistant')
+    screen.geometry("500x500")
+    screen.resizable(width=False, height=False)
+
+    microphone_photo = PhotoImage(file="./lib/gifloader.gif")
+    microphone_button = Button(image=microphone_photo, command=takeCommand())
+    # microphone_button = Button(image=microphone_photo)
+    microphone_button.pack(pady=0)
+
+    screen.mainloop()
+
+
 if __name__ == '__main__':
-    # main method for executing
-    # the functions
-    Take_query()
+    main_screen()
+    # Take_query()
